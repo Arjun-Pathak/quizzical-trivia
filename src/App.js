20421f11e7eb813584589ce5727521774e742a55
Arjun Vivek Pahtak <pathakaruna123@gmail.com>
2022-03-11 11:21:26 +0530

    Started project and initialized with basic startup files and made Startup component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'; // importing stylesheet
+
+// importing components
+import Startup from './components/Startup'
+
+
+// Main app component
+export default function App() {
+  return (
+    <main className={'app'}>
+      <Startup/>
+    </main>
+  );
+}
+
---
+++ src/components/Startup.js
@@ -0,0 +1,9 @@
+
+// startup component
+export default function Startup() {
+	return(
+		<div>
+			<h1>Hello World</h1>
+		</div>
+	);
+}
