ae93d55d42b5f3758a8086064fb8450617364628
Arjun Vivek Pahtak <pathakaruna123@gmail.com>
2022-03-11 12:11:07 +0530

    Startup component completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+// importing necessary functions and packages
+import {useState, useEffect} from 'react';
+
+// importing files
 import './App.css'; // importing stylesheet
+import BlobBlue from './shared/BlobBlue.svg';
+import BlobYellow from './shared/BlobYellow.svg';
 
 // importing components
 import Startup from './components/Startup'
 
-
 // Main app component
 export default function App() {
+
+  // defining state here
+  const [quizPageToggle, setQuizPageToggle] = useState(false); // initializing this state for toggling between startup page and main quiz page
+
   return (
     <main className={'app'}>
-      <Startup/>
+      <img src={BlobYellow} className={'blob-yellow'} alt={'BlobYellow'}/>
+      <Startup toggleQuiz={()=>setQuizPageToggle(true)}/>
+      <img src={BlobBlue} className={'blob-blue'} alt={'BlobBlue'}/>
     </main>
   );
 }
--- src/components/Startup.js
+++ src/components/Startup.js
@@ -1,9 +1,13 @@
 
+import './styles/Startup.css'; // importing stylesheet
+
 // startup component
-export default function Startup() {
+export default function Startup({toggleQuiz}) {
 	return(
-		<div>
-			<h1>Hello World</h1>
+		<div className={'startup'}>
+			<h1 className={'startup--title'}>Quizzical</h1>
+			<p className={'startup--description'}>Welcome is Quizzical Trivia! <br/> Start the quiz by click the button below.</p>
+			<button className={'startup--button'} onClick={toggleQuiz}>Start Quiz</button>
 		</div>
 	);
 }
