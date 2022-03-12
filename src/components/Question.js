0d21da5db44078d2c1851830355297d23779b3e7
Arjun Vivek Pahtak <pathakaruna123@gmail.com>
2022-03-12 09:29:35 +0530

    Completed working of quiz and question component with raw data and styled them completely

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,26 @@
 import './styles/Question.css' // importing stylesheet
-
+import {nanoid} from "nanoid";
 // question component
 export default function Question(props) {
-
 	return(
 		<div className={'question'}>
 			<h3 className={'question-title'}>{props.question}</h3>
 			<div className={'question-button-container'}>
-
-				<button className={'question-button question-button-selected'}>Adios</button>
+				{
+					props.options.map((option)=>	{
+						return <button
+							className={
+							`question-button
+							${props.hasAnswerShown ? 'question-button-not_selected': ''}
+							${!props.hasAnswerShown && props.userAnswer===option ? 'question-button-selected': ''}
+							${props.hasAnswerShown && props.userAnswer === option && props.userAnswer === props.correctAnswer ? 'question-button-correct' : ''}
+							${props.hasAnswerShown && props.userAnswer === option && props.userAnswer!==props.correctAnswer ? 'question-button-wrong' : ''}
+							`}
+							key={nanoid()}
+							onClick={()=> props.changeAnswer(props.id, option)}
+						>
+							{option}
+						</button>})}
 			</div>
 		</div>
 	)
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,6 +2,7 @@ import './styles/Quiz.css'; // importing stylesheet
 // importing functions and packages
 import {useState,useEffect} from 'react';
 import {nanoid} from "nanoid";
+import Confetti from 'react-confetti'
 import data from './data.json';
 
 // importing components
@@ -10,39 +11,57 @@ import Question from './Question'
 // quiz component
 export default function Quiz() {
 	// function to randomize options
-	function randomArrayShuffle(array) {
-		let currentIndex = array.length, temporaryValue, randomIndex;
-		while (0 !== currentIndex) {
-			randomIndex = Math.floor(Math.random() * currentIndex);
-			currentIndex -= 1;
-			temporaryValue = array[currentIndex];
-			array[currentIndex] = array[randomIndex];
-			array[randomIndex] = temporaryValue;
-		}
-		return array;
-	}
 
-	const [questions, setQuestions] = useState(data.results);
 
+	const [questions, setQuestions] = useState([]);
+	const [hasAnswerShown, setAnswerShown] = useState(false);
+	const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
 	useEffect(()=>{
 		const questionArray = [];
 		for(let i = 0; i<data.results.length; i++){
 			const optionArray = [data.results[i].correct_answer, ...data.results[i].incorrect_answers]
+			const id = nanoid();
 			const questionObject = {
 				question: data.results[i].question,
-				options: randomArrayShuffle(optionArray),
-				id: nanoid(),
+				options: optionArray,
+				correctAnswer: data.results[i].correct_answer,
+				userAnswer: '',
+				id: id,
 			}
 			questionArray.push(questionObject)
 		}
 		setQuestions(questionArray)
 	},[]);
-	const QuestionElements = questions.map((question) => <Question key={question.id} {...question} />);
-	console.log(questions[1])
+
+	// function to change user's answer for every quesiton
+	function changeAnswer (id, userAnswer) {
+		if(!hasAnswerShown)
+		setQuestions(oldQuestions=> oldQuestions.map(question=>{
+			return question.id===id ? {...question, userAnswer} : question;
+		}));
+	}
+
+	// function to display how many answers user has gotten correct
+	function displayAnswer () {
+		setAnswerShown(true);
+		questions.map(question=>{
+			setCorrectAnswersCount(prevCount=>{
+				question.userAnswer===question.correctAnswer ? console.log('+1') : console.log("-1");
+				return question.userAnswer===question.correctAnswer ? prevCount+1 : prevCount;
+			})
+		});
+
+	}
+
 	return (
 		<div className={'quiz'}>
-			{QuestionElements}
-			<button className={'quiz-submit'}>Check Answers</button>
+			{hasAnswerShown && correctAnswersCount === 5 && <Confetti/>}
+			{questions.map((question) => {
+				return <Question {...question} hasAnswerShown={hasAnswerShown} key={question.id} changeAnswer={changeAnswer}/>})}
+			<div className="quiz-result-container">
+				{hasAnswerShown && <h4 className={'quiz-result'}>You scored {correctAnswersCount}/5 correct answers</h4>}
+				<button className={'quiz-submit'} onClick={displayAnswer}>Check Answers</button>
+			</div>
 		</div>
 	)
 }
